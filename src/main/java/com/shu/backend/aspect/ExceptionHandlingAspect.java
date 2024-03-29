eb2f55af7d81ac45c79bc8789fcfddf7d0a57ac7
FCBleomessi10 <710823459@qq.com>
2024-03-29 13:24:36 +0800

    chore(project-config): update project configuration
    - Inherit log4j2 in pom.xml for better logging support
    - Modify global exception handling for more robust error management
    - Add MybatisPlus pagination plugin to improve data handling efficiency
    - Implement CORS to enable cross-origin requests
    - Customize log output format for improved logging clarity

--- src/main/java/com/shu/backend/aspect/ExceptionHandlingAspect.java
+++ src/main/java/com/shu/backend/aspect/ExceptionHandlingAspect.java
@@ -1,5 +1,6 @@
 package com.shu.backend.aspect;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shu.backend.vo.response.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -10,18 +11,24 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
+@Slf4j
 @Component
 @Aspect
-@Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ExceptionHandlingAspect {
+
+    @Resource
+    private ObjectMapper objectMapper;
 
     @Around("execution(* com.shu.backend.controller..*(..))")
     public Object handleControllerExceptions(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
             return joinPoint.proceed();
         } catch (Exception e) {
-            log.error("服务器异常, method = {}, Error message = {}", joinPoint.getSignature().getName(), e.getMessage());
+            log.error("服务器异常, Error message = {} args = {}",
+                    e.getMessage(), objectMapper.writeValueAsString(joinPoint.getArgs()));
             return CommonResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), "服务器异常");
         }
     }
